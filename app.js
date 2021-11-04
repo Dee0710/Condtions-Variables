e12eee4a2227e604e1076d7754f43d6206628b38
dee0710 <doris.buffalo@gmail.com>
2021-11-04 14:59:46 -0600

    ConditionsNVariables

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("Hello World!");
+
+var userAge = 17;
+if(userAge >= 18) {
+console.log("The user is over 18!");
+} else {
+    console.log("The user is not old enough!");
+}
+
+
+var userAge = 18;
+var username = "is_subscribed";
+
+if(userAge >= 18 && username == "is_subscribed") {
+    console.log("The user is at least 18 And NOT is_subscribed");
+} else if(userAge < 18 && username === "is_subscribed") {
+    console.log("The user is under 18 AND is_subscribed!");{
+}  if(userAge < 18 && username !== "is_subscribed");{
+    }
+        console.log("The user is under 18 AND is_subscriber");{
+} 
+} else { 
+        console.log("The user is NOT subscribed!");
+    }
